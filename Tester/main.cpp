783e69d58706c335cb2798c6cd4c0694349d7dfe
adesh.lad@cctech.co.in <adesh.lad@cctech.co.in>
2024-01-06 21:00:35 +0530

    Final commit of the day

--- Tester/main.cpp
+++ Tester/main.cpp
@@ -14,27 +14,21 @@ int main()
 	std::vector<Triangle> triangles;
 
 	Reader reader;
-	//reader.readOBJ("F://adesh_workspace//MeshClipping//Reader//resources//bunny.obj", triangles);
 	reader.readSTL("F://adesh_workspace//MeshClipping//Reader//resources//flowerpot.stl", triangles);
 
-	Triangulation triangulation(triangles);
+	Triangulation mesh(triangles);
 
 	Plane plane;
 	plane.setPointOnPlane(Point3D(2, 2, 1));
 	plane.setNormal(Point3D(2, 3, -1));
-	//plane.setConstant(7);
 
-	//Point3D p1(-6, 0, 4);
-	//Point3D p2(4.64264, 11.01722, 7.53331);
+	Clipper clipper;
 
-	Clipper clipper;	
- 	//Point3D	pt = clipper.linePlaneIntersection(p1, p2, plane);
-	Triangulation newTriangulation = clipper.clipWithPlane(triangulation, plane);
-
-	Boundary boundary = clipper.triangulationPlaneIntersection(triangulation, plane);
+	Triangulation clippedMesh = clipper.clipWithPlane(mesh, plane);
+	Boundary boundary = clipper.triangulationPlaneIntersection(mesh, plane);
 
 	Writer writer;
-	writer.write("F://adesh_workspace//MeshClipping//Writer//resources//op1.txt", newTriangulation);
+	writer.write("F://adesh_workspace//MeshClipping//Writer//resources//op1.txt", clippedMesh);
 	writer.write("F://adesh_workspace//MeshClipping//Writer//resources//op2.txt", boundary);
 
 	std::cout << "done";
